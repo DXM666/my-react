a817b934428718dba62b203433f341001dfd1181
DXM666 <846373685@qq.com>
2025-01-04 18:39:41 +0800

    feat:add update

--- packages/react-reconciler/fiber.ts
+++ packages/react-reconciler/fiber.ts
@@ -1,6 +1,9 @@
 import { Props, Key, Ref } from 'shared/ReactTypes';
 import { WorkTag } from './workTags';
 import { Flags, NoFlags } from './fiberFlags';
+import { Container } from 'react-dom/hostConfig';
+
+type FiberNodeType = { tag: WorkTag; pendingProps: Props; key: Key };
 
 export class FiberNode {
 	type: any;
@@ -16,10 +19,12 @@ export class FiberNode {
 	index: any;
 
 	memoizedProps: Props | null;
+	memoizedState: any;
 	alternate: FiberNode | null;
 	flags: Flags;
+	updateQueue: any;
 
-	constructor(tag: WorkTag, pendingProps: Props, key: Key) {
+	constructor({ tag, pendingProps, key }: FiberNodeType) {
 		this.tag = tag;
 		this.key = key || null;
 		this.stateNode = null;
@@ -35,8 +40,54 @@ export class FiberNode {
 		// 作为工作单元
 		this.pendingProps = pendingProps;
 		this.memoizedProps = null;
+		this.memoizedState = null;
+		this.updateQueue = null;
+
 		this.alternate = null;
 		// 副作用
 		this.flags = NoFlags;
 	}
 }
+
+export class FiberRootNode {
+	container: Container;
+	current: FiberNode;
+	finishedWork: FiberNode | null;
+	// pendingChildren: FiberNode | null;
+	constructor(container: Container, hostRootFiber: FiberNode) {
+		this.container = container;
+		this.current = hostRootFiber;
+		this.finishedWork = null;
+		// this.pendingChildren = null;
+	}
+}
+
+export const createWorkInProgress = (
+	current: FiberNode,
+	pendingProps: Props
+) => {
+	let workInProgress = current.alternate;
+	if (workInProgress === null) {
+		// mount
+		workInProgress = new FiberNode({
+			tag: current.tag,
+			pendingProps,
+			key: current.key
+		}); // current.key 为 null 时会触发判断 current.key === pendingProps.key 的逻辑,导致报错
+		workInProgress.stateNode = current.stateNode;
+
+		workInProgress.alternate = current;
+		current.alternate = workInProgress;
+	} else {
+		// update
+		workInProgress.pendingProps = pendingProps;
+		workInProgress.flags = NoFlags;
+	}
+	workInProgress.type = current.type;
+	workInProgress.stateNode = current.stateNode;
+	workInProgress.updateQueue = current.updateQueue;
+	workInProgress.child = current.child;
+	workInProgress.memoizedProps = current.memoizedProps;
+	workInProgress.memoizedState = current.memoizedState;
+	return workInProgress;
+};
---
+++ packages/react-reconciler/fiberReconciler.ts
@@ -0,0 +1,36 @@
+import { Container } from 'react-dom/hostConfig';
+import { ReactElementType } from 'shared/ReactTypes';
+import { FiberNode, FiberRootNode } from './fiber';
+import {
+	createUpdate,
+	createUpdateQueue,
+	enqueueUpdate,
+	UpdateQueue
+} from './updateQueue';
+import { scheduleUpdateOnFiber } from './workLoop';
+import { HostRoot } from './workTags';
+
+export function createContainer(container: Container) {
+	const hostRootFiber = new FiberNode({
+		tag: HostRoot,
+		pendingProps: null,
+		key: null
+	});
+	const root = new FiberRootNode(container, hostRootFiber);
+	hostRootFiber.updateQueue = createUpdateQueue();
+	return root;
+}
+
+export function updateContainer(
+	element: ReactElementType,
+	root: FiberRootNode
+) {
+	const hostRootFiber = root.current;
+	const update = createUpdate<ReactElementType | null>(element);
+	enqueueUpdate(
+		hostRootFiber.updateQueue as UpdateQueue<ReactElementType | null>,
+		update
+	);
+	scheduleUpdateOnFiber(hostRootFiber);
+	return element;
+}
